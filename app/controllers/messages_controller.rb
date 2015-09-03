c869e91076a475cac3c690b3366aa2c7243dc3f7
kotasyan <asya81jp@gmail.com>
2015-09-03 18:33:44 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,24 @@
 class MessagesController < ApplicationController
   def index
+    # messagesテーブルからすべてのレコードを取得
+    @messages = Message.all
+  end
+  
+  def create
+    # Messageモデルのインスタンス作成
+    # 引数：formにて入力された:name, :body（ハッシュ）
+    @message = Message.new(message_params)
+    # MessageモデルのインスタンスをDBに保存
+    @message.save
+    redirect_to root_path , notice: 'メッセージを保存しました'
+  end
+
+  # ここから下はprivateメソッド
+  private
+  def message_params
+    # params[:message]のパラメータで name , bodyのみを許可
+    # 返り値は ex:) {name: "入力されたname" , body: "入力されたbody" }
+    # 許可されたパラメータ以外が渡された場合の挙動は？--デフォルトでは無視。
+    params.require(:message).permit(:name, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  # messagesコントローラとindexアクションをルートに指定
   root 'messages#index'
+  # RESTfulなURLを自動生成 (POST /messages)
+  resources :messages, only: [:create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
