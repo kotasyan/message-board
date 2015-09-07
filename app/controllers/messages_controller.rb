8dd835934c9e7a6158a75497f08e38a2cd9c25ce
kotasyan <asya81jp@gmail.com>
2015-09-07 17:11:24 +0000

    Implementation of the validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,15 +2,24 @@ class MessagesController < ApplicationController
   def index
     # messagesテーブルからすべてのレコードを取得
     @messages = Message.all
+    # 空のメッセージオブジェクトを生成
+    @message = Message.new
   end
   
   def create
     # Messageモデルのインスタンス作成
     # 引数：formにて入力された:name, :body（ハッシュ）
     @message = Message.new(message_params)
-    # MessageモデルのインスタンスをDBに保存
-    @message.save
-    redirect_to root_path , notice: 'メッセージを保存しました'
+    # MessageモデルのインスタンスをDBに保存(保存できない場合はfalseを返す)
+    if @message.save
+      redirect_to root_path , notice: 'メッセージを保存しました'
+    else
+      #メッセージが保存できなかった時
+      @messages = Message.all
+      #アラートを表示（現在のアクションのみ）
+      flash.now[:alert] = "メッセージの保存に失敗しました"
+      render 'index'
+    end
   end
 
   # ここから下はprivateメソッド
